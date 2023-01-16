b8c8307d2acd4326358c91f2babf65f7b4c0b32e
Nitigya Joshi <77828908+nitigya-joshi@users.noreply.github.com>
2023-01-16 22:24:00 +0530

    Updated prot numbers in env file

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
-require('dotenv').config()
-const express=require('express')
-const path=require('path')
-const router=require('./routes')
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+const router = require("./routes");
 
+const app = express();
+const cors = require("cors");
 
-const app=express()
-const cors=require('cors')
+app.use(cors());
+const PORT = process.env.PORT || 3001;
+app.use(express.static(path.join(__dirname, "public")));
+app.use(router);
 
-app.use(cors())
-const PORT=process.env.API_PORT
-app.use(express.static(path.join(__dirname, 'public')))
-app.use(router)
-
-app.listen(PORT,(err)=>{
-    console.log("API server running at port 3001")
-})
+app.listen(PORT, (err) => {
+	console.log("API server running at port 3001");
+});
